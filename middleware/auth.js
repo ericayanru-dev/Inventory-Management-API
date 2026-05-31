5799ba76bce5c24dc45716393d1bb71c6ede106d
ericayanru-dev <ericayanru@outlook.com>
2026-05-31 20:09:19 +0100

    Support both cookies and headers

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
     try {
-      const token = req.cookies.token;
+const token = req.cookies.token || req.header("Authorization")?.replace("Bearer ", "");
 
         if (!token) {
             return res.status(401).json({ message: "No token provided" });
